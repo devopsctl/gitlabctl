f20d5c48d88db1867bd5824d38f485a8bba98bbe
John Carino <depwaswho94@gmail.com>
2018-05-22 22:57:52 +0800

    Refactor and add more test data

--- testdata/seeder.sh
+++ testdata/seeder.sh
@@ -4,14 +4,29 @@ echo "=============================================="
 echo "#              SEEDER SCRIPT                 #"
 echo "=============================================="
 
+#set gitlab base url
+base_url="${1}/"
+
+# get access token
+access_token=$(curl -X POST $base_url"oauth/token?grant_type=password&username=${2}&password=${3}" | jq '.access_token' | tr -d '"')
+
+# create user
+echo "Creating a user"
+curl -X POST $base_url"api/v4/users?access_token=$access_token&name=John+Doe&email=john.doe@gmail.com&username=john.doe&password=123qwe123&skip_confirmation=true"
+
 # create group
 echo "Creating a group"
-pgroup_id=$(curl --header "Private-Token: $GITLAB_PRIVATE_TOKEN" -X POST "http://localhost:10080/api/v4/groups?name=DevOps&path=DevOps" | jq '.id')
+pgroup_id=$(curl -X POST $base_url"api/v4/groups?access_token=$access_token&name=DevOps&path=DevOps" | jq '.id')
+
+# create group project
+echo "Creating a group project"
+curl -X POST $base_url"api/v4/projects?name=DevOpsProject&namespace_id=$pgroup_id&access_token=$access_token"
 
 # create subgroup
 echo "Creating a subgroup"
-sgroup_id=$(curl --header "Private-Token: $GITLAB_PRIVATE_TOKEN" -X POST "http://localhost:10080/api/v4/groups?name=SecOps&path=SecOps&parent_id=${pgroup_id}" | jq '.id')
+sgroup_id=$(curl -X POST $base_url"api/v4/groups?access_token=$access_token&name=SecOps&path=SecOps&parent_id=$pgroup_id" | jq '.id')
 
-# display groups in gitlab instance
-curl --header "Private-Token: $GITLAB_PRIVATE_TOKEN" "http://localhost:10080/api/v4/groups" | jq '.'
+# create subgroup project
+echo "Creating a subgroup project"
+curl -X POST $base_url"api/v4/projects?access_token=$access_token&name=SecOpsProject&namespace_id=$sgroup_id"
 
