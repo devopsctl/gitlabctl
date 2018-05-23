74cc5f1aa214a0895dc16a25517f8f42d4f3c795
John Bryan Sazon <bryansazon@hotmail.com>
2018-05-23 21:13:54 +0800

    Refactor the helper functions according to file names and add the 'new group' flags too.

---
+++ cmd/flag_helpers.go
@@ -0,0 +1,148 @@
+// Copyright © 2018 github.com/devopsctl authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	gitlab "github.com/xanzy/go-gitlab"
+)
+
+// addGetGroupsFlags adds common flags for `get groups` and `get subgroups` commands
+func addGetGroupsFlags(cmd *cobra.Command) {
+	cmd.Flags().Bool("all-available", false,
+		"Show all the groups you have access to "+
+			"(defaults to false for authenticated users, true for admin)")
+	cmd.Flags().Bool("owned", false,
+		"Limit to groups owned by the current user")
+	cmd.Flags().Bool("statistics", false,
+		"Include group statistics (admins only)")
+	cmd.Flags().String("sort", "asc",
+		"Order groups in asc or desc order. Default is asc")
+	cmd.Flags().String("search", "",
+		"Return the list of authorized groups matching the search criteria")
+	cmd.Flags().String("order-by", "name",
+		"Order groups by name or path. Default is name")
+}
+
+// addNewGroupFlags adds common flags for `new group` or `edit group` commands
+func addNewGroupFlags(cmd *cobra.Command) {
+	addNameFlag(cmd)
+	addNamespaceFlag(cmd)
+	addLFSenabled(cmd)
+	addRequestsAccessenabledFlag(cmd)
+	addVisibilityFlag(cmd)
+	addJSONFlag(cmd)
+	if err := cmd.MarkFlagRequired("name"); err != nil {
+		er(err)
+	}
+}
+
+//
+// NOTE(@bzon): All addFlags* helpers should be added below
+//
+
+func addNameFlag(cmd *cobra.Command) {
+	cmd.Flags().String("name", "", "resource name")
+}
+
+func addNamespaceFlag(cmd *cobra.Command) {
+	cmd.Flags().String("namespace", "",
+		"The parent group id or group path if creating a subgroup. "+
+			"(defaults to current user namespace)")
+}
+
+func addVisibilityFlag(cmd *cobra.Command) {
+	cmd.Flags().String("visibility", "", "public, internal or private")
+}
+
+func addRequestsAccessenabledFlag(cmd *cobra.Command) {
+	cmd.Flags().Bool("request-access-enabled", false, "enable request access")
+}
+
+func addLFSenabled(cmd *cobra.Command) {
+	cmd.Flags().Bool("lfs-enabled", false, "enable LFS")
+}
+
+func addPathFlag(cmd *cobra.Command) {
+	cmd.Flags().String("path", "",
+		"the group name, id or full the path "+
+			"including the parent group (path/to/group)")
+	if err := cmd.MarkFlagRequired("path"); err != nil {
+		panic(err)
+	}
+}
+
+func addJSONFlag(cmd *cobra.Command) {
+	cmd.Flags().Bool("json", false, "print the command output to json")
+}
+
+//
+// NOTE(@bzon): All getFlag* helpers should be added below
+//
+
+func getFlagString(cmd *cobra.Command, flag string) string {
+	s, err := cmd.Flags().GetString(flag)
+	if err != nil {
+		glog.Fatalf("error accessing flag %s for command %s: %v",
+			flag, cmd.Name(), err)
+	}
+	return s
+}
+
+func getFlagBool(cmd *cobra.Command, flag string) bool {
+	b, err := cmd.Flags().GetBool(flag)
+	if err != nil {
+		glog.Fatalf("error accessing flag %s for command %s: %v",
+			flag, cmd.Name(), err)
+	}
+	return b
+}
+
+func getFlagInt(cmd *cobra.Command, flag string) int {
+	i, err := cmd.Flags().GetInt(flag)
+	if err != nil {
+		glog.Fatalf("error accessing flag %s for command %s: %v",
+			flag, cmd.Name(), err)
+	}
+	return i
+}
+
+// getFlagVisibility converts the string flag visiblity to gitlab.VisibilityValue.
+// It will exit with error if the vibility flag given by the user is not valid.
+func getFlagVisibility(cmd *cobra.Command) (*gitlab.VisibilityValue, error) {
+	// TODO(@bzon): can this be improved?
+	v := getFlagString(cmd, "visibility")
+	gv := gitlab.VisibilityValue(v)
+	switch gv {
+	case gitlab.PrivateVisibility:
+		return gitlab.Visibility(gv), nil
+	case gitlab.PublicVisibility:
+		return gitlab.Visibility(gv), nil
+	case gitlab.InternalVisibility:
+		return gitlab.Visibility(gv), nil
+	default:
+		// user error
+		return nil, fmt.Errorf("visibility valid values are only (public, private, internal)")
+	}
+}
--- cmd/group_helpers.go
+++ cmd/group_helpers.go
@@ -1,3 +1,23 @@
+// Copyright © 2018 github.com/devopsctl authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package cmd
 
 import (
@@ -9,6 +29,76 @@ import (
 	"github.com/spf13/cobra"
 	gitlab "github.com/xanzy/go-gitlab"
 )
+
+// getListGroupsOptions maps the cmd flags to gitlab.ListGroupsOptions struct.
+// It also ensures that the struct field that is associated with the command
+// flag does not use the flag default value.
+func getListGroupsOptions(cmd *cobra.Command) *gitlab.ListGroupsOptions {
+	var opts gitlab.ListGroupsOptions
+	if cmd.Flag("all-available").Changed {
+		opts.AllAvailable = gitlab.Bool(getFlagBool(cmd, "all-available"))
+	}
+	if cmd.Flag("owned").Changed {
+		opts.Owned = gitlab.Bool(getFlagBool(cmd, "owned"))
+	}
+	if cmd.Flag("statistics").Changed {
+		opts.Statistics = gitlab.Bool(getFlagBool(cmd, "statistics"))
+	}
+	if cmd.Flag("sort").Changed {
+		opts.Sort = gitlab.String(getFlagString(cmd, "sort"))
+	}
+	if cmd.Flag("search").Changed {
+		opts.Search = gitlab.String(getFlagString(cmd, "search"))
+	}
+	if cmd.Flag("order-by").Changed {
+		opts.OrderBy = gitlab.String(getFlagString(cmd, "order-by"))
+	}
+	return &opts
+}
+
+// getCreateGroupOptions maps the cmd flags to gitlab.CreateGroupOptions struct.
+// It also ensures that the struct field that is associated with the command
+// flag does not use the flag default value.
+func getCreateGroupOptions(cmd *cobra.Command) *gitlab.CreateGroupOptions {
+	var opts gitlab.CreateGroupOptions
+	opts.Path = gitlab.String(getFlagString(cmd, "name"))
+	opts.Name = opts.Path
+	if cmd.Flag("namespace").Changed {
+		ns := getFlagString(cmd, "namespace")
+		id, err := strconv.Atoi(ns)
+		// if not nil take the given number
+		if err == nil {
+			opts.ParentID = &id
+		}
+		// find the group as string and get it's id
+		opts.ParentID = getGroupID(getFlagString(cmd, "namespace"))
+	}
+	if cmd.Flag("visibility").Changed {
+		v, err := getFlagVisibility(cmd)
+		if err != nil {
+			er(err)
+		}
+		opts.Visibility = v
+	}
+	if cmd.Flag("lfs-enabled").Changed {
+		opts.LFSEnabled = gitlab.Bool(getFlagBool(cmd, "lfs-enabled"))
+	}
+	if cmd.Flag("request-access-enabled").Changed {
+		opts.RequestAccessEnabled = gitlab.Bool(
+			getFlagBool(cmd, "request-access-enabled"))
+	}
+	return &opts
+}
+
+// get the groupID of a group
+// exits with error if group does not exist
+func getGroupID(path string) *int {
+	g, err := descGroup(path)
+	if err != nil {
+		er(err)
+	}
+	return gitlab.Int(g.ID)
+}
 
 // printGroupsOut prints the group list/get commands to a table view or json
 func printGroupsOut(cmd *cobra.Command, groups ...*gitlab.Group) {
