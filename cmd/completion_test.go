1f37aafa2b14f36828cbe873a5da31b75b018c85
JB Sazon <bryansazon@hotmail.com>
2018-06-15 07:32:04 +0800

    Add completion command

---
+++ cmd/completion_test.go
@@ -0,0 +1,73 @@
+// Copyright © 2018 github.com/devopsctl authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompletion(t *testing.T) {
+	tt := []struct {
+		name     string
+		flagsMap map[string]string
+		expect   testResult
+	}{
+		{
+			name: "generate bash completions",
+			flagsMap: map[string]string{
+				"bash": "true",
+			},
+			expect: pass,
+		},
+		{
+			name: "generate zsh completions",
+			flagsMap: map[string]string{
+				"zsh": "true",
+			},
+			expect: pass,
+		},
+		{
+			name: "generate zsh and bash completions at the same time fails",
+			flagsMap: map[string]string{
+				"bash": "true",
+				"zsh":  "true",
+			},
+			expect: fail,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			execT := execTestCmdFlags{
+				t:        t,
+				cmd:      completionCmd,
+				flagsMap: tc.flagsMap,
+				noParent: true,
+			}
+			stdout, execResult := execT.executeCommand()
+			tInfo(stdout)
+			require.Equal(t, tc.expect, execResult,
+				printFlagsTable(tc.flagsMap, stdout))
+		})
+	}
+}
